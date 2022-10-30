c217ed9819d92103f77fea4568c64a91dc03a842
LelouchLamperougee <pavel.bv2003@gmail.com>
2022-10-30 11:08:49 +0300

    bootstrap and vue connected to the project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        vue(),
+        laravel([
+            'resources/js/app.js',
+        ]),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
